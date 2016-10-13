8db97d5f042540127f0976430cc7f315d4b77b27
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2016-10-13 16:14:20 +0200

    Usage of gridFS for storing grid_calendars

--- tartare/interfaces/grid_calendar.py
+++ tartare/interfaces/grid_calendar.py
@@ -102,15 +102,8 @@ class GridCalendar(Resource):
         if not valid_header:
             return {'message': 'non-compliant file(s) : {}'.format(''.join(invalid_files))}, 400
 
-        # backup content
-        input_dir = coverage.technical_conf.input_dir
-        if not os.path.exists(input_dir):
-            os.makedirs(input_dir)
         content.stream.seek(0)
-        content.save(os.path.join(input_dir, content.filename + ".tmp"))
+        coverage.save_grid_calendars(content)
         zip_file.close()
-        full_file_name = os.path.join(os.path.realpath(input_dir), content.filename)
-
-        shutil.move(full_file_name + ".tmp", full_file_name)
 
         return {'message': 'OK'}, 200
