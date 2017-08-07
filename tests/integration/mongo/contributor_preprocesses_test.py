9e66ab7aab49e56e273ce6d6840f285bb453fecd
Nicolas Berard <nicolas.berard@canaltp.fr>
2017-08-07 11:53:16 +0200

    update existing tests

--- tests/integration/mongo/contributor_preprocesses_test.py
+++ tests/integration/mongo/contributor_preprocesses_test.py
@@ -27,29 +27,28 @@
 # https://groups.google.com/d/forum/navitia
 # www.navitia.io
 
-from tartare.processes.contributor import GtfsAgencyFile
-from tartare.core.context import Context, DataSourceContext, ContributorContext
-from tartare.core.models import Contributor, ValidityPeriod
-from tartare.core.gridfs_handler import GridFsHandler
 from datetime import date
-from tartare import app
-from tartare.exceptions import ParameterException
 from zipfile import ZipFile
-from tartare.core.calendar_handler import get_dict_from_zip
-from gridfs.errors import NoFile
-import pytest
-import os
 
-preprocess = {
-    "data_source_ids": ["id2"],
-    "params": {
-        "data": {
-            "agency_id": "112",
-            "agency_name": "stif",
-            "agency_url": "http://stif.com"
-        }
+import pytest
+from gridfs.errors import NoFile
+
+from tartare import app
+from tartare.core.calendar_handler import get_dict_from_zip
+from tartare.core.context import Context, DataSourceContext, ContributorContext
+from tartare.core.gridfs_handler import GridFsHandler
+from tartare.core.models import Contributor, ValidityPeriod, PreProcess
+from tartare.exceptions import ParameterException
+from tartare.processes.contributor import GtfsAgencyFile
+from tests.validity_period_finder_test import _get_file_fixture_full_path
+
+preprocess = PreProcess(sequence=0, data_source_ids=["id2"], type="GtfsAgencyFile", params={
+    "data": {
+        "agency_id": "112",
+        "agency_name": "stif",
+        "agency_url": "http://stif.com"
     }
-}
+})
 
 excepted_headers = ["agency_id", "agency_name", "agency_url", "agency_timezone", "agency_lang",
                     "agency_phone", "agency_fare_url", "agency_email"]
@@ -57,22 +56,20 @@ excepted_headers.sort()
 
 
 class TestContributorProcesses:
-
     def get_gridfs_id(self, filename, contributor_id='contrib_id'):
-        fixtures_path = os.path.realpath('tests/fixtures/gtfs/{filename}')
-        with open(fixtures_path.format(filename=filename), 'rb') as file:
+        with open(_get_file_fixture_full_path('gtfs/{filename}'.format(filename=filename)), 'rb') as file:
             return GridFsHandler().save_file_in_gridfs(file, filename=filename, contributor_id=contributor_id)
 
     def manage_gtfs_and_get_context(self, gridfs_id, context, data_source_id='id2', contributor_preprocess=None):
-            contributor = Contributor('123', 'ABC', 'abc')
-            validity_period = ValidityPeriod(date(2018, 7, 1), date(2018, 7, 1))
+        contributor = Contributor('123', 'ABC', 'abc')
+        validity_period = ValidityPeriod(date(2018, 7, 1), date(2018, 7, 1))
 
-            data_source_context = DataSourceContext(data_source_id, gridfs_id, validity_period)
-            contributor_context = ContributorContext(contributor, [data_source_context], validity_period)
-            context.contributor_contexts.append(contributor_context)
-            pr = contributor_preprocess if contributor_preprocess else preprocess
-            gtfs_agency_file = GtfsAgencyFile(context, pr)
-            gtfs_agency_file.do()
+        data_source_context = DataSourceContext(data_source_id, gridfs_id, validity_period)
+        contributor_context = ContributorContext(contributor, [data_source_context], validity_period)
+        context.contributor_contexts.append(contributor_context)
+        pr = contributor_preprocess if contributor_preprocess else preprocess
+        gtfs_agency_file = GtfsAgencyFile(context, pr)
+        gtfs_agency_file.do()
 
     def test_gtfs_without_agency_file(self):
         with app.app_context():
@@ -86,7 +83,7 @@ class TestContributorProcesses:
             assert gridfs_id != new_gridfs_id
 
             with pytest.raises(NoFile) as excinfo:
-                    GridFsHandler().get_file_from_gridfs(gridfs_id)
+                GridFsHandler().get_file_from_gridfs(gridfs_id)
             assert str(excinfo.value).startswith('no file in gridfs collection')
 
             new_gridfs_file = GridFsHandler().get_file_from_gridfs(new_gridfs_id)
@@ -98,7 +95,8 @@ class TestContributorProcesses:
                 keys = list(data[0].keys())
                 keys.sort()
                 assert keys == excepted_headers
-                for key, value in preprocess.get("params").get("data").items():
+
+                for key, value in preprocess.params.get("data").items():
                     assert value == data[0][key]
 
     def test_gtfs_with_agency_file(self):
@@ -130,7 +128,7 @@ class TestContributorProcesses:
             assert gridfs_id != new_gridfs_id
 
             with pytest.raises(NoFile) as excinfo:
-                    GridFsHandler().get_file_from_gridfs(gridfs_id)
+                GridFsHandler().get_file_from_gridfs(gridfs_id)
             assert str(excinfo.value).startswith('no file in gridfs collection')
 
             new_gridfs_file = GridFsHandler().get_file_from_gridfs(new_gridfs_id)
@@ -142,7 +140,7 @@ class TestContributorProcesses:
                 keys = list(data[0].keys())
                 keys.sort()
                 assert keys == excepted_headers
-                for key, value in preprocess.get("params").get("data").items():
+                for key, value in preprocess.params.get("data").items():
                     assert value == data[0][key]
 
     def test_gtfs_with_data_source_not_in_context(self):
@@ -150,7 +148,7 @@ class TestContributorProcesses:
             context = Context()
             gridfs_id = self.get_gridfs_id(filename="gtfs_empty_agency_file.zip")
             with pytest.raises(ParameterException) as excinfo:
-                    self.manage_gtfs_and_get_context(gridfs_id=gridfs_id, context=context, data_source_id='id15')
+                self.manage_gtfs_and_get_context(gridfs_id=gridfs_id, context=context, data_source_id='id15')
             assert str(excinfo.value).startswith('impossible to build preprocess GtfsAgencyFile : '
                                                  'data source id2 not exist for contributor 123')
 
@@ -158,9 +156,7 @@ class TestContributorProcesses:
         with app.app_context():
             context = Context()
             gridfs_id = self.get_gridfs_id(filename="gtfs_empty_agency_file.zip")
-            contributor_preprocess = {
-                "data_source_ids": ["id2"]
-            }
+            contributor_preprocess = PreProcess(data_source_ids=["id2"])
 
             self.manage_gtfs_and_get_context(gridfs_id=gridfs_id, context=context,
                                              contributor_preprocess=contributor_preprocess)
@@ -171,7 +167,7 @@ class TestContributorProcesses:
             assert gridfs_id != new_gridfs_id
 
             with pytest.raises(NoFile) as excinfo:
-                    GridFsHandler().get_file_from_gridfs(gridfs_id)
+                GridFsHandler().get_file_from_gridfs(gridfs_id)
             assert str(excinfo.value).startswith('no file in gridfs collection')
 
             new_gridfs_file = GridFsHandler().get_file_from_gridfs(new_gridfs_id)
