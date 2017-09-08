7139f8af26563919a6c40ab556480aef9e87d60a
Nicolas Berard <nicolas.berard@canaltp.fr>
2017-09-08 15:41:56 +0200

    clean root files in container

--- run_functional_tests.sh
+++ run_functional_tests.sh
@@ -7,5 +7,6 @@ export TARTARE_HOST_IP=$(docker inspect --format '{{range .NetworkSettings.Netwo
 export HTTP_SERVER_IP=$(docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'  $(docker-compose -f docker-compose.test.yml ps -q http_download_server))
 py.test -vv  tests/functional
 RESULT=$?
+docker exec -it $(docker-compose -f docker-compose.test.yml ps -q http_download_server) rm -rf /var/www/.temp
 docker-compose -f docker-compose.test.yml down
 exit $RESULT
