89eb3090654742f7396b5aa95a40238c1ea30c10
Nicolas Berard <nicolas.berard@canaltp.fr>
2018-02-08 14:59:07 +0100

    add publiser and uploader tests

---
+++ tests/integration/publisher_test.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2001-2016, Canal TP and/or its affiliates. All rights reserved.
+#
+# This file is part of Navitia,
+#     the software to build cool stuff with public transport.
+#
+# Hope you'll enjoy and contribute to this project,
+#     powered by Canal TP (www.canaltp.fr).
+# Help us simplify mobility and open public transport:
+#     a non ending quest to the responsive locomotion way of traveling!
+#
+# LICENCE: This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Stay tuned using
+# twitter @navitia
+# IRC #navitia on freenode
+# https://groups.google.com/d/forum/navitia
+# www.navitia.io
+
+import pytest
+
+from tartare.core.constants import PLATFORM_TYPE_NAVITIA, PLATFORM_PROTOCOL_FTP, PLATFORM_PROTOCOL_HTTP, \
+    PLATFORM_TYPE_ODS
+from tartare.core.models import Platform
+from tartare.core.publisher import PublisherManager, AbstractPublisher, ProtocolManager, AbstractProtocol
+from tartare.exceptions import PublisherManagerException, ProtocolManagerException
+
+
+class TestPublisher:
+    def test_select_from_platform_ok(self):
+        publisher = PublisherManager.select_from_platform(
+            Platform(PLATFORM_PROTOCOL_FTP, PLATFORM_TYPE_NAVITIA, 'ftp://whatever'))
+        assert isinstance(publisher, AbstractPublisher)
+
+    def test_select_from_platform_ko(self):
+        with pytest.raises(PublisherManagerException) as excinfo:
+            PublisherManager.select_from_platform(Platform(PLATFORM_PROTOCOL_FTP, 'wrong_type', 'ftp://whatever'))
+        assert excinfo.typename == 'PublisherManagerException'
+        assert str(excinfo.value) == 'unknown platform type "wrong_type"'
+
+
+class TestUploader:
+    def test_select_from_platform_ok(self):
+        uploader = ProtocolManager.select_from_platform(
+            Platform(PLATFORM_PROTOCOL_HTTP, PLATFORM_TYPE_NAVITIA, 'ftp://whatever'))
+        assert isinstance(uploader, AbstractProtocol)
+
+    def test_select_from_platform_ko(self):
+        with pytest.raises(ProtocolManagerException) as excinfo:
+            ProtocolManager.select_from_platform(Platform('wrong_protocol', PLATFORM_TYPE_ODS, 'ftp://whatever'))
+        assert excinfo.typename == 'ProtocolManagerException'
+        assert str(excinfo.value) == 'unknown platform protocol "wrong_protocol"'
